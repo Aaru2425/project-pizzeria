01a8010f8090b2027eceaf43f2a0eca40ca9e2df
Aaru2425 <89341865+Aaru2425@users.noreply.github.com>
2021-09-11 16:51:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+menu_list_array = ["Veg Margherita Pizza","paneer pizza","pineapple pizza","double cheese burst pizza","chocolate lava cake"
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+          
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+ menu_list_array.sort();
+        htmldata='<section class="cards">'
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+"<div class ='cards'>" +"<img src ='pizzaImg.png'>" +menu_list_array[i]+'</div>'
+        }
+        htmldata=htmldata+'</section>'
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
